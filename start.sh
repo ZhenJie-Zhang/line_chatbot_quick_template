d0e45941e205c55bafb250f0577d70f813fd5798
ZhenJie-Zhang <abc1233108@gmail.com>
2019-11-04 14:19:34 +0000

    update app.ipynb

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ sudo docker-compose up -d
 sleep 5s
 sudo curl $(docker port ngrok 4040)/api/tunnels > tunnels.json
 sudo docker run -v $(pwd)/tunnels.json:/tmp/tunnels.json --rm  realguess/jq jq .tunnels[1].public_url /tmp/tunnels.json 
-sudo docker exec -it jupyter bash
+sudo docker exec -it line-chat-bot-jupyter bash
 cd /root; jupyter notebook --allow-root &
---
+++ user_tweets_realtime_scoresForLineChat_.py
@@ -0,0 +1,22 @@
+﻿from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+from afinn import Afinn
+
+if __name__ == "__main__":
+
+    sc = SparkContext()
+    ssc = StreamingContext(sc, 1)
+
+    # A sentiment analysis model
+    model = Afinn()
+
+    raw_stream = KafkaUtils.createStream(ssc, "zookeeper:2181", "consumer-group", {"tweets_stream": 1}) \
+                           .window(60, 5)
+
+    raw_stream.pprint(20)
+
+
+    # Start it
+    ssc.start()
+    ssc.awaitTermination()
